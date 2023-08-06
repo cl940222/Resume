c6ffbe51c121def59580b0795079ea80947a88d7
cl940222 <cl940222@gmail.com>
2023-08-06 12:00:33 +0800

    8/6 12:00

--- js/all.js
+++ js/all.js
@@ -1,3 +1,6 @@
+
+
+
 // sideBar JS scripts >>
 
 function openNav() {
@@ -8,7 +11,7 @@ function openNav() {
     document.getElementById("secExp").style.marginLeft = "300px";
     document.getElementById("secWork").style.marginLeft = "300px";
     document.getElementById("secContact").style.marginLeft = "300px";
-    
+
 }
 
 function closeNav() {
@@ -22,13 +25,110 @@ function closeNav() {
 }
 
 
-$(function(){
-    $('#main').on('mouseover', function(){
-        $('#mySidebar:nth-child(3)').css('color','rgb(241, 241, 241)');
-        console.log('ok');
-        // $('#mySidebar').find('a:nth-child(3)').css('color', '')
+// sideBar JS scripts <<
+
+
+// 回到上方按鈕>>
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $('#myBtn').css('display','block');
+    } else {
+        $('#myBtn').css('display','none');
+    }
+}
+window.onscroll = function () { scrollFunction() };
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// 回到上方按鈕<<
+
+
+
+$(function () {
+
+
+    // 聯絡我們-點擊複製>>
+
+    $('#copyPhone, #copyMail').on({
+        mouseover: function () {
+            $(this).css({
+                color: 'rgb(113, 159, 150)',
+                cursor: 'pointer',
+            })
+        },
+        mouseout: function () {
+            $(this).css('color', 'rgb(242, 177, 123)');
+
+        },
+        click: function () {
+            navigator.clipboard.writeText($(this).find('span').text());
+            $(this).removeClass('bi-clipboard');
+            $(this).addClass('bi-check2');
+            $(this).on('mouseout', function () {
+                $(this).removeClass('bi-check2');
+                $(this).addClass('bi-clipboard');
+            });
+
+        }
     })
 
-})
 
-// sideBar JS scripts <<
+
+
+
+    // 聯絡我們-點擊複製<<
+
+    // 聯絡我們 表單填寫框的樣式>>
+
+
+
+
+    // 聯絡我們 表單填寫框的樣式<<
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
